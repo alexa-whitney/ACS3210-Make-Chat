1d9634fd3fcf595cf3749b0d5439abb22d6fcd47
alexa-whitney <alexawhitney@gmail.com>
2023-06-09 19:32:05 -0600

    Implemented changing channels

--- sockets/chat.js
+++ sockets/chat.js
@@ -12,7 +12,7 @@ module.exports = (io, socket, onlineUsers, channels) => {
 
     socket.on('new message', (data) => {
         //Save the new message to the channel.
-        channels[data.channel].push({sender : data.sender, message : data.message});
+        channels[data.channel].push({ sender: data.sender, message: data.message });
         //Emit only to sockets that are in that channel room.
         io.to(data.channel).emit('new message', data);
     });
@@ -39,6 +39,15 @@ module.exports = (io, socket, onlineUsers, channels) => {
         //Inform all clients of the new channel.
         io.emit('new channel', newChannel);
         //Emit to the client that made the new channel, to change their channel to the one they made.
+        socket.emit('user changed channel', {
+            channel: newChannel,
+            messages: channels[newChannel]
+        });
+    });
+
+    //Have the socket join the room of the channel
+    socket.on('user changed channel', (newChannel) => {
+        socket.join(newChannel);
         socket.emit('user changed channel', {
             channel: newChannel,
             messages: channels[newChannel]
