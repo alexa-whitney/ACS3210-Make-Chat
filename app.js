0735616c9c005e28a60231563dce254c6f870c62
alexa-whitney <alexawhitney@gmail.com>
2023-06-09 18:05:01 -0600

    Users can log in

--- app.js
+++ app.js
@@ -6,11 +6,12 @@ const server = require('http').Server(app);
 //Socket.io
 const io = require('socket.io')(server);
 io.on("connection", (socket) => {
-  console.log("🔌 New user connected! 🔌");
+  // This file will be read on new socket connections
+  require('./sockets/chat.js')(io, socket);
 });
 
 //Express View Engine for Handlebars
-const exphbs  = require('express-handlebars');
+const exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.use('/public', express.static('public'));
